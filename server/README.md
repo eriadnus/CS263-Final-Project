eede34d9f196ba6022dab3214adc53085a747a3a
Albert Stanley <albertjstanley@gmail.com>
2023-06-04 13:23:32 -0700

    starting server

--- server/README.md
+++ server/README.md
@@ -1,2 +1,5 @@
 # Notes
 run test.py to generate results that will be pushed onto output.json in the background
+
+# Run server
+python3 server.py
--- server/server.py
+++ server/server.py
@@ -34,3 +34,6 @@ def get_sentiment_analysis():
         mimetype='application/json',
     )
     return response
+
+if __name__ == "__main__":
+    app.run()
